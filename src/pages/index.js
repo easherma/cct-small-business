7d062469e6a175786e26648575364b3c1ad42512
Eric Sherman <ericandrewsherman@gmail.com>
2020-06-23 11:01:50 -0500

    fix: remove test bootstrap code

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,24 +15,6 @@ const IndexPage = () => (
       <Image />
     </div>
  <section>
-  <div className="container">
-    <div className="row align-items-center">
-      <div className="col-lg-6 order-lg-2">
-        <div className="p-5">
-          <img className=" img-fluid" src="assets/img/Barriers.png" />
-        </div>
-      </div>
-      <div className="col-lg-6 order-lg-1">
-        <div className="p-5">
-          <p>
-            Black and Latinx-owned businesses comprise just 8% of the total
-            number of small businesses combined.
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
 
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
