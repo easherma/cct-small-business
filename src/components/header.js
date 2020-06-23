1518953bbd7f1bbff2afe48f57c208959c045d75
Eric Sherman <ericandrewsherman@gmail.com>
2020-06-23 15:15:02 -0500

    add: simple hacky header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,16 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import titleImage from "../images/illustrations/title-homepage-eng.png"
 
-const Header = ({ siteTitle }) => (
+const Header = () => (
   <header>
     <div>
-      <h1>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+        <img src={titleImage} />
+        <h1>Creating a marketplace of services for entrepreneurs of color to close the racial wealth gap and build a more just, equitable and resilient society.</h1>
+        <h2>A collaboration between The Chicago Community Trust,  The Coleman Foundation, JPMorgan Chase Foundation, Robert R. McCormick Foundation, Leslie Bluhm and David Helfand, Peter and Lucy Ascoli Family Fund, and the Liz and Don Thompson Family Fund.</h2>  
     </div>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
