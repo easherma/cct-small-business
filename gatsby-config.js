eb3d5de91f2ad8f61d086deaf5075cf2f149b5c1
Eric Sherman <ericandrewsherman@gmail.com>
2020-06-24 14:17:21 -0500

    add: working example pulling data from json

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,15 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-         // This path is relative to the root of the site.
+        // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `./src/content/`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
