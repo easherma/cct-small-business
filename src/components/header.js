f07c4046e111848bcaffd441304e56b5ac6427db
Eric Sherman <ericandrewsherman@gmail.com>
2020-06-23 20:01:25 -0500

    first hacky stab at component

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,7 @@ const Header = () => (
     <div>
         <Image src={titleImage} alt="The Fund for Equitable Business Growth" className="img-responsive" fluid />
         <h1 className="lead">Creating a marketplace of services for entrepreneurs of color to close the racial wealth gap and build a more just, equitable and resilient society.</h1>
+        
         <p>A collaboration between The Chicago Community Trust,  The Coleman Foundation, JPMorgan Chase Foundation, Robert R. McCormick Foundation, Leslie Bluhm and David Helfand, Peter and Lucy Ascoli Family Fund, and the Liz and Don Thompson Family Fund.</p>  
     </div>
     </Container>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
+import Layout from '../components/layout'
+import Image from '../components/image'
+import SEO from '../components/seo'
+import FourColumnBlock from '../components/four-column-block'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <FourColumnBlock />
   </Layout>
 )
 
