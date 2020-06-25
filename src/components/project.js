3925a370cd672c4b01a6a3a4413488bbabb1fcec
Antonio Foushee <AntonioStacy@gmail.com>
2020-06-25 14:30:59 -0400

    added container

--- src/components/project.js
+++ src/components/project.js
@@ -2,12 +2,14 @@ import React from "react"
 import Card from "react-bootstrap/Card"
 import ReactMarkdown from "react-markdown"
 import Linkify from "react-linkify"
-import Img from "gatsby-image"
+import Container from "react-bootstrap/Container"
+
 // some of our fields will need to be markdown and parsed,
 // in a good project we'd define and validate these datatypes,
 // but we might not have time for that
 
 const Project = ({
+
   image,
   title,
   primaryPartner,
@@ -16,21 +18,22 @@ const Project = ({
   additionalDescription,
   callToAction,
 }) => (
-  <Card>
-    <Img fluid={image} alt="" />
-    <Card.Title>{title}</Card.Title>
-    <Card.Body>
-      <Card.Text>{primaryPartner}</Card.Text>
-      <Card.Text>{otherPartners}</Card.Text>
-      <Card.Text>{description}</Card.Text>
-      <Card.Text>
-        <ReactMarkdown source={additionalDescription} />
-      </Card.Text>
-      <Card.Text>
-        <Linkify>{callToAction}</Linkify>
-      </Card.Text>
-    </Card.Body>
-  </Card>
-)
-
+    <Container>
+      <Card>
+        <Card.Img variant="top" src={image} />
+        <Card.Title>{title}</Card.Title>
+        <Card.Body>
+          <Card.Text>{primaryPartner}</Card.Text>
+          <Card.Text>{otherPartners}</Card.Text>
+          <Card.Text>{description}</Card.Text>
+          <Card.Text>
+            <ReactMarkdown source={additionalDescription} />
+          </Card.Text>
+          <Card.Text>
+            <Linkify>{callToAction}</Linkify>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Container>
+  )
 export default Project
