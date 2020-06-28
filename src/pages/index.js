153fc2a1429fc18f4fde1c6224d9d4c11299105e
Antonio Foushee <AntonioStacy@gmail.com>
2020-06-27 20:40:47 -0400

    images added

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,67 @@ const IndexPage = () => {
   // at some point we might refactor it, esp when we implement spanish
   const images = useStaticQuery(graphql`
     {
+      smallBusinessesOfColor: file(
+        relativePath: { eq: "images/small-businesses-of-color.png" }
+      ) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      chiSmallBusiness: file(
+        relativePath: { eq: "images/chi-sml-bus.png" }
+      ) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      barriers: file(relativePath: { eq: "images/barriers.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      open: file(relativePath: { eq: "images/blk-ltx-own.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      blackAndWhiteOwned: file(relativePath: { eq: "images/blk-wht-own.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      buenaEnglewood: file(relativePath: { eq: "images/buena-park-and-englewood.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
       vision: file(relativePath: { eq: "images/our-vision.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      businessPlan: file(relativePath: { eq: "images/notepad.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      awardedPartnerships: file(relativePath: { eq: "images/map-2.png" }) {
         childImageSharp {
           fluid {
             ...GatsbyImageSharpFluid
@@ -33,50 +93,60 @@ const IndexPage = () => {
           }
         }
       }
-      barriers: file(relativePath: { eq: "images/barriers.png" }) {
-        childImageSharp {
-          fluid {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
     }
   `)
 
   return (
     <Layout>
       <SEO title="Home" />
-
       <SectionHeader
-        imageSource={images.partnerships.childImageSharp.fluid}
-        altText="Explore The Partnerships"
-
+        imageSource={images.smallBusinessesOfColor.childImageSharp.fluid}
+        altText="Small Businesses of Color"
       />
       <StoryBlock
-        imageSource={images.barriers.childImageSharp.fluid}
-        altText="Barriers"
-        description="Hellow word!"
-      />
-      <StoryBlock
-        imageSource={images.barriers.childImageSharp.fluid}
-        altText="Barriers"
+        imageSource={images.chiSmallBusiness.childImageSharp.fluid}
+        altText="Chicago Small Businesse"
         description="Hellow word!"
         imageRight
       />
-      <StoryBlocks />
+      <StoryBlock
+        imageSource={images.barriers.childImageSharp.fluid}
+        altText="Barriers"
+        description="Hellow word!"
+      />
+      <StoryBlock
+        imageSource={images.open.childImageSharp.fluid}
+        altText="Black and Latinx-Owned Businesses"
+        description="Hellow word!"
+      />
+      <StoryBlock
+        imageSource={images.blackAndWhiteOwned.childImageSharp.fluid}
+        altText="Black and White-Owned Businesses"
+        description="Hellow word!"
+      />
+      <StoryBlock
+        imageSource={images.buenaEnglewood.childImageSharp.fluid}
+        altText="Buena Park and Englewood"
+        description="Hellow word!"
+      />
       <SectionHeader
         imageSource={images.vision.childImageSharp.fluid}
         altText="Our Vision"
       />
-      <OurVisionStoryBlocks />
+      <OurVisionStoryBlocks imageSource={images.businessPlan.childImageSharp.fluid}
+        altText="Business Plans"
+        description="Hellow word!"
+      />
+      <OurVisionStoryBlocks imageSource={images.awardedPartnerships.childImageSharp.fluid}
+        altText="Explore The Partnerships"
+        description="Hellow word!"
+      />
       <FourColumnBlock />
       <SectionHeader
         imageSource={images.partnerships.childImageSharp.fluid}
         altText="Explore The Partnerships"
       />
-
       <Projects />
-
     </Layout>
   )
 }
